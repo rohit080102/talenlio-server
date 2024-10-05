f1df4d6e7157282bfa4fa3bf9c00f6aae8da63b6
rohit080102 <rohitshettigar0801@gmail.com>
2024-10-06 02:42:44 +0530

    first comit

---
+++ src/main/java/com/bezcoder/server/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.bezcoder.server.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bezcoder.server.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+}
